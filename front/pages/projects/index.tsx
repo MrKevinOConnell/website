7a3484d25ec89c4cd438afcc54785e7809c7bac2
Kevin O'Connell <kevinoconnell42@gmail.com>
2022-11-14 20:31:12 -0600

    copy

--- front/pages/projects/index.tsx
+++ front/pages/projects/index.tsx
@@ -17,7 +17,7 @@ import ProjectCard from '../../components/ProjectCard'
 export default function projects () {
   return (
     <Stack align="center">
-        <Text size={25} weight="700">Things I have worked on</Text>
+        <Text size={25} weight="700">Projects</Text>
      <ProjectCard soon={true} title="FarcasterFM (soon)" body={'Music Port of Farcaster'} url="https://www.farcaster.xyz/"/>
      <ProjectCard soon={false} title="PostHog" body="Open source product analytics software" url="https://posthog.com/"/>
      <ProjectCard soon={false} title="ForestRun" body="Infinite runner game + scary maze game in one" url="https://www.forestrun.xyz/"/>
